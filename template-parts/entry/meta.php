2b5bf125a5900e7da5c685d32688b6f4f457d3f8
suri <suri>
2017-03-13 15:42:37 +0530

    Issue#16: User-formatted date

--- template-parts/entry/meta.php
+++ template-parts/entry/meta.php
@@ -21,15 +21,15 @@
 	<span<?php manta_attr( 'posted-on' ) ?>>
 		<a href="<?php the_permalink(); ?>" rel="bookmark">
 			<?php if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) :?>
-				<time datetime="<?php the_modified_date( 'c' ) ?>"<?php manta_attr( 'modified-entry-date' )?>>
-					<?php the_modified_date( 'M j, Y' ); ?>
+				<time datetime="<?php the_modified_date( DATE_W3C ) ?>"<?php manta_attr( 'modified-entry-date' )?>>
+					<?php the_modified_date(); ?>
 				</time>
-				<time datetime="<?php echo esc_attr( get_the_date( 'c' ) ) ?>"<?php manta_attr( 'entry-date' )?>>
-					<?php echo esc_html( get_the_date( 'M j, Y' ) ); ?>
+				<time datetime="<?php echo esc_attr( get_the_date( DATE_W3C ) ) ?>"<?php manta_attr( 'entry-date' )?>>
+					<?php echo esc_html( get_the_date() ); ?>
 				</time>
 			<?php else : ?>
-				<time datetime="<?php echo esc_attr( get_the_date( 'c' ) ) ?>"<?php manta_attr( 'entry-date' )?>>
-					<?php echo esc_html( get_the_date( 'M j, Y' ) ); ?>
+				<time datetime="<?php echo esc_attr( get_the_date( DATE_W3C ) ) ?>"<?php manta_attr( 'entry-date' )?>>
+					<?php echo esc_html( get_the_date() ); ?>
 				</time>
 			<?php endif;?>
 		</a>
